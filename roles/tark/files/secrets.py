8d21d69d8df40937f4baf6c005fdd9315e18738b
Matthew Laird <lairdm@ebi.ac.uk>
2017-08-22 17:34:51 +0100

    More tweaking... - Tark installs - Nodes know how to ssh to each other - mysql is functioning with the cached database volume

---
+++ roles/tark/files/secrets.py
@@ -0,0 +1,11 @@
+import env
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = 'mysecretkey'
+
+DATABASE_USER = 'anonymous'
+DATABASE_PASSWORD = None
+DATABASE_HOST = 'mysql'
+DATABASE_PORT = '3306'
+    
+TARK_DATABASE = 'tark'
